49a1a1e68ff5f36ba8a0fa1e4bfeabc104bfde4a
Stefan Richardson <stefan.richardson@qhrtech.com>
2017-10-20 01:07:54 -0600

    set up for panel landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   animate,
   transition
 } from '@angular/animations';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,27 @@ import {
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  id: string;
 
+  panelIds = [
+    {development: false, isActive: false, isOpen: false},
+    {design: false, isActive: false, isOpen: false},
+    {games: false, isActive: false, isOpen: false},
+    {about: false, isActive: false, isOpen: false},
+    {shop: false, isActive: false, isOpen: false}
+  ];
+
+  toggleOpen(i) {
+    console.log(this.panelIds[i]);
+    this.panelIds[i].isOpen = !this.panelIds[i].isOpen;
+    Observable.interval(1000)
+    .takeWhile(() => !this.id)
+    .subscribe(x => { 
+        console.log(x);
+        this.panelIds[i].isActive = !this.panelIds[i].isActive;
+        if (x < 1) {this.id = 'STOP!!';}
+    });
+
+    this.id = undefined;
+  }
 }
