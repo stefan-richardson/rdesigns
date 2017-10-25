cbc7acf0653ab23d05b5f846a61e23c6e44a2e1b
Stefan Richardson <stefan.richardson@qhrtech.com>
2017-10-24 21:23:28 -0600

    yarn erased all material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,52 +5,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DragScrollModule } from 'angular2-drag-scroll';
 // COMPONENTS
 import { AppComponent } from './app.component';
-import { WebDesignSectionComponent } from './web-design/web-design-section/web-design-section.component';
-import { WebDesignFeaturesComponent } from './web-design/web-design-features/web-design-features.component';
-import { GraphicDesignSectionComponent } from './graphic-design/graphic-design-section/graphic-design-section.component';
-import { IllustrationSectionComponent } from './illustrations/illustration-section/illustration-section.component';
-import { AboutSectionComponent } from './about/about-section/about-section.component';
-import { ShopSectionComponent } from './shop/shop-section/shop-section.component';
-
-// SERVICES
-import { SectionToggleService } from './services/section-toggle.service';
 
 // OTHER IMPORTS
-import {
-  MdSnackBarModule, MdIconModule, MdInputModule, MdToolbarModule,
-  MdCardModule, MdButtonModule, MdDialogModule, MdMenuModule, MdSidenavModule
-} from '@angular/material';
 import 'hammerjs';
-import { FooterComponent } from './shared/footer/footer.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WebDesignSectionComponent,
-    GraphicDesignSectionComponent,
-    IllustrationSectionComponent,
-    AboutSectionComponent,
-    ShopSectionComponent,
-    WebDesignFeaturesComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     DragScrollModule,
-    MdInputModule, MdToolbarModule, MdCardModule, MdIconModule, MdDialogModule,
-    MdButtonModule, MdMenuModule, MdSnackBarModule, MdSidenavModule,
     AppRoutingModule, // AppRoutingModule should ALWAYS last here to ensure the wild card route '**' is the last.
   ],
-  providers: [SectionToggleService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
