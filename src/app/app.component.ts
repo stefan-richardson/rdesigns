670fa88c7f3cf29d23ff5304798d95977a6d20ec
Stefan Richardson <stefan.richardson@qhrtech.com>
2017-10-24 20:59:14 -0600

    landing page set up with filler images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,27 +15,35 @@ import {Observable} from 'rxjs/Rx';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  id: string;
 
   panelIds = [
-    {development: false, isActive: false, isOpen: false},
-    {design: false, isActive: false, isOpen: false},
-    {games: false, isActive: false, isOpen: false},
-    {about: false, isActive: false, isOpen: false},
-    {shop: false, isActive: false, isOpen: false}
+    {title: 'Development', message: 'Have a Look!', class: 'panel1', isActive: false, isOpen: false},
+    {title: 'Design', message: 'Come see!', class: 'panel2', isActive: false, isOpen: false},
+    {title: 'Games', message: 'Wanna Play?', class: 'panel3', isActive: false, isOpen: false},
+    {title: 'About', message: 'Check Me Out!', class: 'panel4', isActive: false, isOpen: false},
+    {title: 'Shop', message: 'Buy Stuff!', class: 'panel5', isActive: false, isOpen: false}
   ];
 
-  toggleOpen(i) {
-    console.log(this.panelIds[i]);
-    this.panelIds[i].isOpen = !this.panelIds[i].isOpen;
-    Observable.interval(1000)
-    .takeWhile(() => !this.id)
-    .subscribe(x => { 
-        console.log(x);
-        this.panelIds[i].isActive = !this.panelIds[i].isActive;
-        if (x < 1) {this.id = 'STOP!!';}
-    });
+  isDev;
+  isDesign;
+  isGames;
+  isAbout;
+  isShop;
 
-    this.id = undefined;
+  panelControl(i) {
+    this.toggleOpen(i);
+    Observable.interval(1000)
+    .take(1)
+    .subscribe(_ => {
+        this.toggleActive(i);
+    });
+  }
+
+  toggleOpen(i) {
+    this.panelIds[i].isOpen = !this.panelIds[i].isOpen;
+  }
+
+  toggleActive(i) {
+    this.panelIds[i].isActive = !this.panelIds[i].isActive;
   }
 }
