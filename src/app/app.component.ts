3e5cc982afeea4ac1b128724762fe9dea80658f7
Stefan Richardson <stefan.richardson@qhrtech.com>
2017-10-24 21:03:59 -0600

    Landing page set up (#1)
    * set up for panel landing page
    
    * landing page set up with filler images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   animate,
   transition
 } from '@angular/animations';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,34 @@ import {
 })
 export class AppComponent {
 
+  panelIds = [
+    {title: 'Development', message: 'Have a Look!', class: 'panel1', isActive: false, isOpen: false},
+    {title: 'Design', message: 'Come see!', class: 'panel2', isActive: false, isOpen: false},
+    {title: 'Games', message: 'Wanna Play?', class: 'panel3', isActive: false, isOpen: false},
+    {title: 'About', message: 'Check Me Out!', class: 'panel4', isActive: false, isOpen: false},
+    {title: 'Shop', message: 'Buy Stuff!', class: 'panel5', isActive: false, isOpen: false}
+  ];
+
+  isDev;
+  isDesign;
+  isGames;
+  isAbout;
+  isShop;
+
+  panelControl(i) {
+    this.toggleOpen(i);
+    Observable.interval(1000)
+    .take(1)
+    .subscribe(_ => {
+        this.toggleActive(i);
+    });
+  }
+
+  toggleOpen(i) {
+    this.panelIds[i].isOpen = !this.panelIds[i].isOpen;
+  }
+
+  toggleActive(i) {
+    this.panelIds[i].isActive = !this.panelIds[i].isActive;
+  }
 }
